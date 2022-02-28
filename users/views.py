3cf9ea8176fc3048dac23c38de7abf1b34a04df0
Kamlesh Prajapati <kamleshprajapati0405@gmail.com>
2022-02-28 22:54:47 +0530

    Added register and login functionality

--- users/views.py
+++ users/views.py
@@ -1,20 +1,20 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+# from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from .forms import RegistrationForm
 # Create your views here.
 
 def register(request):
 
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = RegistrationForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data['username']
             messages.success(request, f'Welcome {username}, your account is created successfully.')
             return redirect('food:index')
     else:
-        #     return redirect('register')
-        form = UserCreationForm()
+        form = RegistrationForm()
     return render(request, 'users/register.html', {
         'form': form
     })
