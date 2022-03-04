324d80b5c774375a0499168fbe73733e747d46d4
Kamlesh Prajapati <kamleshprajapati0405@gmail.com>
2022-03-04 22:31:07 +0530

    added profile functionality for users

--- foodiee/settings.py
+++ foodiee/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-fiif@++grmis7o&(tgq1uvx=ifxa%7k1u@p63qn@ll03(!5a!h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost']
 
 
 # Application definition
@@ -129,3 +130,6 @@ LOGIN_REDIRECT_URL = 'food:index'
 
 # Redirect the annonymous users for login
 LOGIN_URL = 'login'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'pictures')
+MEDIA_URL = '/pictures/'
--- users/views.py
+++ users/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 # from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from .forms import RegistrationForm
+from django.contrib.auth.decorators import login_required
+
 # Create your views here.
 
 def register(request):
@@ -18,3 +20,8 @@ def register(request):
     return render(request, 'users/register.html', {
         'form': form
     })
+
+@login_required
+def profile(request):
+
+    return render(request, 'users/profile.html')
