3466f1647c1a5e4ea3a3946f48111d04faca1d24
Kamlesh Prajapati <kamleshprajapati0405@gmail.com>
2022-03-09 23:19:00 +0530

    Added user field for item and some class based view

--- food/urls.py
+++ food/urls.py
@@ -8,9 +8,11 @@ urlpatterns = [
     path('', views.IndexClassView.as_view(), name='index'),
     # path('item/', views.item, name='item'),
     # details page
-    path('<int:item_id>/', views.detail, name='detail'),
+    # path('<int:item_id>/', views.detail, name='detail'),
+    path('<int:pk>/', views.FoodDetail.as_view(), name='detail'),
     # add items
-    path('add/', views.create_item, name='create_item'),
+    # path('add/', views.create_item, name='create_item'),
+    path('add/', views.CreateItem.as_view(), name='create_item'),
     # update item
     path('update/<int:item_id>/', views.update_item, name='update_item'),
     # delete item
--- food/views.py
+++ food/views.py
@@ -6,6 +6,9 @@ from .forms import ItemForm
 
 from django.contrib.auth.decorators import login_required
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
+from django.utils.decorators import method_decorator
 
 # Create your views here.
 
@@ -32,20 +35,42 @@ class IndexClassView(ListView):
 def detail(request, item_id):
     item = Item.objects.get(pk=item_id)
     return render(request, 'food/details.html', {
-        'item': item
+        'object': item
     })
+
+@method_decorator(login_required, name='dispatch')
+class FoodDetail(DetailView):
+
+    model = Item
+    template_name = 'food/details.html'
 
 @login_required
 def create_item(request):
     form = ItemForm(request.POST or None)
 
     if form.is_valid():
+
+        # updating the item_created_by field before saving in db
+        form.instance.item_created_by = request.user
         form.save()
-        return redirect('food:index')
+        return redirect(form.instance.get_absolute_url())
 
     return render(request, 'food/item-form.html', {
         'form': form
     })
+
+@method_decorator(login_required, name='dispatch')
+class CreateItem(CreateView):
+
+    model = Item
+    fields = ['item_name', 'item_desc', 'item_price', 'item_image']
+    template_name = 'food/item-form.html'
+
+    def form_valid(self, form):
+        form.instance.item_created_by = self.request.user
+        return super().form_valid(form)
+
+
 
 @login_required
 def update_item(request, item_id):
